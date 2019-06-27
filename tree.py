cb62ddc0d788f68ae5baaf394a523b8309fa23dd
HCelante <higor.celante@gmail.com>
2019-06-27 13:11:58 -0300

    Update tree.py
    insercao rec

--- tree.py
+++ tree.py
@@ -19,9 +19,9 @@ class Tree:
         if (self.root == None): # se o no raiz for nulo
             self.root = novo # insere o no na raiz
         else: # se o nó a ser inserido nao for raiz
-            atual = self.root
+            atual = self.root # variavel para percorrer os nos da arvore
             while True:
-                anterior = atual # variavel para percorrer os nos da arvore
+                anterior = atual # variavel para guardar a posicao anterior
                 if valor <= atual.item: # caminha para a esquerda
                     atual = atual.esq # variavel que percorre a arvore recebe o no da esquerda
                     if atual == None: # se for vazio
@@ -32,3 +32,27 @@ class Tree:
                     if atual == None: # se for vazio insere
                         anterior.dir = novo
                         return
+
+    def insererecB(self,valor,atual,anterior,novo):
+        if valor <= atual.item: # caminha para a esquerda
+            atual = atual.esq # variavel que percorre a arvore recebe o no da esquerda
+            if atual == None: # se for vazio
+                anterior.esq = novo # insere na esquerda
+                return 
+        else: # caminha para a direita
+            atual = atual.dir #variavel que percorre a arvore recebe o no da direita
+            if atual == None: # se for vazio insere
+                anterior.dir = novo
+                return
+
+        self.insererecB(valor,atual,anterior,novo)
+
+    def insererec(self,valor):
+        novo = No(valor,None,None) # instancia o novo no e ja insere o valor
+        if (self.root == None): # se o no raiz for nulo
+            self.root = novo # insere o no na raiz
+        else: # se o nó a ser inserido nao for raiz
+            atual = self.root
+            anterior = atual # variavel para guardar a posicao anterior
+            self.insererecB(valor,atual,anterior,novo)
+            return
