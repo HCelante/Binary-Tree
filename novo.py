e7a06bd7ea61594948c896b6821d5ac125217f38
HCelante <higor.celante@gmail.com>
2019-07-11 02:22:06 -0300

    teste de funcoes e correcoes

--- novo.py
+++ novo.py
@@ -1,9 +1,17 @@
+
+
 class No:
      
     def __init__(self, key, dir, esq):
         self.item = key
         self.dir = dir
         self.esq = esq
+
+
+class AVLPronta:
+    def __init__(self):
+        self.root = No(55, No(21,       No(13,None,None),      No(8,None,None))    ,                          No(34,      No(21,None,None)    , No(13,None,None) ))
+                #raiz, filhodireito(filho direito , filho esquerdo), filho esquerdo(filho direito, filho esquerdo)
 
 class Tree:
     
@@ -29,6 +37,7 @@ class Tree:
                     if atual == None: # se for vazio insere
                         anterior.dir = novo
                         return
+    
 
     def buscar(self, chave):
         if self.root == None:
@@ -59,7 +68,7 @@ class Tree:
         
         if ((atual.item <= filhoEsq.item) or (atual.item <= filhoDir.item)):
             return False
-        return self.maxHeap(filhoEsq) and self.maxHeap(filhoDir) 
+        return True and (self.maxHeap(filhoEsq) and self.maxHeap(filhoDir)) 
         
 
     def minHeap(self, root):
@@ -110,9 +119,9 @@ class Tree:
         if atual == None:
             return False # se arvore vazia
         alturaEsq = self.altura(atual.esq)
-        print(alturaEsq)
+        #print(alturaEsq)
         alturaDir = self.altura(atual.dir)
-        print(alturaDir)
+        #print(alturaDir)
         if (abs(alturaEsq - alturaDir) <= 1): 
             return True
         return False
@@ -180,35 +189,100 @@ class Tree:
         self.preOrder(self.root)
 
     def isSum(self,atual):
-        if ((atual.dir == None) and (atual.esq == None)):# quando chega na folha retorna true pois a condicao se manteve verdadeira
-            return True # em toda a arvore
-        if (atual.item == (atual.dir.item + atual.esq.item)):# só avaça na arvore se a condicao se manter verdadeira
-            return self.isSum(atual.dir) and self.isSum(atual.esq) # retorna um and do resultado da subarvore esquerda e da direita
-        else:
-            return False
+
+        if ((atual == None) or (atual.dir == None) or (atual.esq == None)):
+            return
+        #print (atual.item )
+        #print (atual.dir.item + atual.esq.item)
+        if(atual.item == (atual.dir.item + atual.esq.item)):
             
-    def areCousins(self,p1,p2, avo):
-        if(avo == None):
-            return False
-        avo = avo
-        filhoesq = avo.esq
-        filhodir = avo.dir
-        neto1 = filhoesq.esq.item
-        neto2 = filhoesq.dir.item
-        neto3 = filhodir.esq.item
-        neto4 = filhodir.dir.item
+            return True and (self.isSum(atual.dir) and self.isSum(atual.esq))
 
-        if((p1 == (neto1 or neto2)) and (p2 == (neto3 or neto4))):
-            return True
-        if((p1 == (neto1 or neto2)) and (p2 != (neto3 or neto4))) or ((p1 != (neto1 or neto2)) and (p2 == (neto3 or neto4))):
-            return False
         else:
-            return self.areCousins(p1,p2,avo.dir) and self.areCousins(p1,p2,avo.esq)
+            return 
 
- 
+    #    
+    #    if ((atual.dir == None) and (atual.esq == None)):# quando chega na folha retorna true pois a condicao se manteve verdadeira
+    #        return # em toda a arvore
+    #    if (atual.item == (atual.dir.item + atual.esq.item)):# só avaça na arvore se a condicao se manter verdadeira
+    #        return True and (self.isSum(atual.dir) and self.isSum(atual.esq)) # retorna um and do resultado da subarvore esquerda e da direita
+    #    else:
+    #        return False
+            
+    #def areCousins(self,p1,p2, avo):
+    #    if(avo == None):
+    #        return False
+    #    avo = avo
+    #    filhoesq = avo.esq
+    #    filhodir = avo.dir
+    #    neto1 = filhoesq.esq.item
+    #    neto2 = filhoesq.dir.item
+    #    neto3 = filhodir.esq.item
+    #    neto4 = filhodir.dir.item
+#
+    #    if((p1 == (neto1 or neto2)) and (p2 == (neto3 or neto4))):
+    #        return True
+    #    if((p1 == (neto1 or neto2)) and (p2 != (neto3 or neto4))) or ((p1 != (neto1 or neto2)) and (p2 == (neto3 or neto4))):
+    #        return False
+    #    else:
+    #        return self.areCousins(p1,p2,avo.dir) and self.areCousins(p1,p2,avo.esq)
+
+    def areCousins(self,p1,p2,raiz):
+        if(raiz == None or raiz.esq == None or raiz.dir == None or raiz.dir.esq == None or raiz.dir.dir == None):
+            print("retorna")
+            return
+        print("avo = ", raiz.item)
+        Avo = raiz
+        filhoesq = Avo.esq
+        print("filho esq", filhoesq.item)
+        filhodir = Avo.dir
+        print("filho dir", filhodir.item)
+        neto1 = int(filhoesq.esq.item)
+        print("filho esq do filho esq", filhoesq.esq.item)
+        neto2 = int(filhoesq.dir.item)
+        print("filho dir do filho esq", filhoesq.dir.item)
+        neto3 = int(filhodir.esq.item)
+        neto4 = int(filhodir.dir.item)
+        if((p1 == neto1 or p1 == neto2) and (p2 == neto3 or p2 == neto4)):
+            return True
+        if((p1 == neto3 or p1 == neto4) and (p2 == neto1 or p2 == neto2)):
+            return True
+        else:
+            self.areCousins(p1,p2,filhoesq)
+            self.areCousins(p1,p2,filhodir)
+
 
 arv = Tree()
 opcao = 0
+# arvore soma
+print("Teste para arvore Soma")
+#########################################################
+# TESTES
+soma = Tree()
+soma.inserir (55)
+soma.root.dir = No(34,No(21,None,None),No(13,None,None))
+soma.root.esq = No(21, No(13,None,None),No(5,None,None))
+#soma.preOrder(soma.root)
+#print(soma.isSum(soma.root))
+if (soma.isSum(soma.root)):
+    print("Funcao eh Soma OK")
+else:
+    print("ehSoma Falhou")
+if (soma.areCousins(21,5,soma.root)):
+    print("Funcao Sao Primos Ok")
+else: 
+    print("Sao Primos falhou")
+if(soma.isComplete(soma.root,0,soma.contadorNos(soma.root))):
+    print("Funcao isComplete Ok")
+if False == (soma.maxHeap(soma.root)):
+    print("Funcao maxHeap Ok")
+if True == (soma.isAVL(soma.root)):
+    print("Funcao AVL OK")
+
+
+
+# FIM TESTES
+#############################################################################
 while opcao != 3:
     print("\n Entre com a opcao:")
     print("1: Inserir")
@@ -218,6 +292,7 @@ while opcao != 3:
     print("5: AVL")
     print("6: Completo")
     print("7: Sair do programa")
+    print("8: Se for Soma")
     opcao = int(input("-> "))
     if opcao == 1:
         x = int(input("Informe o valor: "))
@@ -240,3 +315,5 @@ while opcao != 3:
         print(complet)
     elif opcao == 7:
         break
+    elif opcao == 8:
+        print(arv.isSum(arv.root))
